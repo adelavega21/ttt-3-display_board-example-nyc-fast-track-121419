fa9fc4623a07d8e619e6acbc9ab3f161fb952baf
Andrew de la Vega <andrew21delavega@gmail.com>
2019-12-14 17:59:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "   ","|","   ","|","   "
+  puts "nil","|","nil","|","nil"
   puts "-----------"
-  puts "   ","|","   ","|","   "
+  puts "nil","|","nil","|","nil"
   puts "-----------"
-  puts "   ","|","   ","|","   "
+  puts "nil","|","nil","|","nil"
 end
 
 display_board
